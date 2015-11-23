02f2992d904d59d67ca46c7ed1ad0ecbf5667de8
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2015-11-23 16:21:47 +0200

    Korjauksia

--- luokitustenAvaus.js
+++ luokitustenAvaus.js
@@ -9,11 +9,11 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 var _ = require('underscore');
 
-var out = fs.createWriteStream('YKL-luokat_avattu.txt');
+var out = fs.createWriteStream('YKL-luokat_avattu_korjattu.txt');
 
 var codeArray = [];
 
-fs.readFile('YKL-luokat.txt', 'utf-8', function (err, data) {
+fs.readFile('testidata.txt', 'utf-8', function (err, data) {
   var splitData = data.split('\n');
   splitData.forEach(function (line) {
     var code = line.trim().split(' ')[1];
@@ -27,7 +27,7 @@ function getAndWriteDefinition(array) {
   var code = array.pop();
   getDefition(code);
   setTimeout(function() {
-    if (codeArray.length > 1) { getAndWriteDefinition(codeArray); }
+    if (codeArray.length > 0) { getAndWriteDefinition(codeArray); }
     else { console.log('Done!'); process.exit(); }
   }, 5000);
 }
@@ -38,13 +38,34 @@ function getDefition(field) {
     if (err) throw err;
     else {
       var $ = cheerio.load(body);
-      var definition = $( '#ctl00_body_ctl00_main_ctl00_sectionTitle > h1' ).text();
-      console.log(definition);
-      out.write(definition + '\n');
+      var definition = $( '#ctl00_body_ctl00_main_ctl00_sectionTitle > h1' ).text().trim();
+      var extra = $( '#ctl00_body_ctl00_main_ctl00_form > div > div.formTag > h3' ).text().trim();
+      var result = '';
+      if (extra) {
+        result += field + '\t' + returnDefinition(definition) + '\t' + getClass(extra);
+      } else {
+        result += field + '\t' + definition;
+      }
+      console.log(result);
+      out.write(result + '\n');
     }
   });
 }
 
 function parseUrl(field) {
   return 'http://ykl.kirjastot.fi/fi-FI/luokat/?MinClassNumber=' + field + '&MaxClassNumber=' + field + '&ClassNumber=' + field;
+}
+
+function returnCode(line) {
+  return line.split(' ')[0];
+}
+
+function returnDefinition(line) {
+  var array = line.split(' ');
+  array.shift();
+  return array.join(' ');
+}
+
+function getClass(string) {
+  return string.split('muotoluokka ')[1];
 }
